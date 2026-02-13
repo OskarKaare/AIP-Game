a0ec2c9fdb81be7b0c09691b39edb63b7da76b29
Oskar B. Kaare <oskar.kaare@gmail.com>
2026-02-13 13:17:19 +0100

    did some

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -9,11 +12,17 @@ public class PlayerStats : MonoBehaviour
     [SerializeField] private int oxygenDepletionRate = 1;
     [SerializeField] private float oxygenDepletionInterval = 1f;
     [SerializeField] private int oxygenReplenishmentRate = 1;
-  
+    [SerializeField] private float depthThreshold = -2.55f;
+    
+    public Volume postProcessVolume;
+    private Vignette vignette;
+
 
     void Start()
     {
         StartCoroutine(ManageOxygen());
+
+        
     }
     private void Update()
     {
@@ -24,7 +33,7 @@ public class PlayerStats : MonoBehaviour
     {
         while (true)
         {
-            if (transform.position.y < -2.55f)
+            if (transform.position.y < depthThreshold)
             {
                 // Deplete oxygen
                 yield return new WaitForSeconds(oxygenDepletionInterval);
@@ -33,8 +42,7 @@ public class PlayerStats : MonoBehaviour
 
                 if (currentOxygen <= 0)
                 {
-                    currentOxygen = 0;
-                    Die();
+                    TakeDamage(10f);
                 }
             }
             else 
@@ -42,14 +50,40 @@ public class PlayerStats : MonoBehaviour
                 yield return new WaitForSeconds (0.25f); 
                 if (currentOxygen < maxOxygen)
                     currentOxygen += oxygenReplenishmentRate;
+                    if (currentOxygen > maxOxygen)
+                        currentOxygen = maxOxygen;
 
             }
         }
     }
 
+    void TakeDamage(float damage)
+    {
+        health -= damage;
+        Debug.Log($"Player took {damage} damage. Current health: {health}");
+        StartCoroutine(DamageFeedback());
+        if (health <= 0)
+        {
+            health = 0;
+            Die();
+        }
+    }
+
+
+    IEnumerator DamageFeedback()
+    {
+        if (postProcessVolume != null && postProcessVolume.profile != null)
+        {
+            postProcessVolume.profile.TryGet<Vignette>(out vignette);
+        }
+        vignette.color.value = Color.red;
+        yield return new WaitForSeconds(1f);
+        vignette.color.value = Color.black;
+    }
     void Die()
     {
         Debug.Log("Player died");
-   
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
     }
 }
